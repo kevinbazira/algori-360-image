413f093fdb406e262f7a4b2f1829f50e3a512260
KEVIN BAZIRA <kvnbzr@gmail.com>
2018-11-27 17:14:34 +0300

    Changed: Updated version number to 1.0.2

--- algori-360-image.php
+++ algori-360-image.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: Algori 360 Image
  * Plugin URI: https://github.com/kevinbazira/algori-360-image/
- * Description: <strong>360 Image</strong> is a Gutenberg Block Plugin that enables you add interactive 360° images to your website. Adding immersive panorama images, spherical images, equirectangular photos, full-sphere 3D images and VR (Virtual Reality) photography <strong>will boost user engagement and increase revenue for your site</strong>.
+ * Description: <strong>Algori 360 Image</strong> is a Gutenberg Block Plugin that enables you add interactive 360° images to your website. Adding immersive panorama images, spherical images, equirectangular photos, full-sphere 3D images and VR (Virtual Reality) photography <strong>will boost user engagement and increase revenue for your site</strong>.
  * Author: Kevin Bazira
  * Author URI: http://kevinbazira.com/
- * Version: 1.0.1
+ * Version: 1.0.2
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
